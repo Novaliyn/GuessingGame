47e27da3ea4e2d4a01e68b786a6fa9664d599eb9
Novaliyn <189510880+Novaliyn@users.noreply.github.com>
2024-12-06 23:56:28 -0800

    converted to TK inter GUI

--- Guessinggame.py
+++ Guessinggame.py
@@ -1,40 +1,68 @@
 import random
+from tkinter import *
+import tkinter.font as tkfont
 
-def guess(): #user input guess funcion
-    global NUMGUESS
-    while NUMGUESS>0:
-        guess = input("Please give me an animal: ")
-        if guess in aws_list:
-            my_list.append(guess)
-            NUMGUESS -= 1
-        else:
-            print(f"{guess} was not an option in the answer list.")
- 
-
-def intro_text(): #starting text
-    print(f"Guess the hidden animal! You have {str(NUMGUESS)} choice(s)")
-    print('Your choices are: ')
-    print(aws_list)
-
-#lists and variables
-my_list = []
-aws_list = ["dog", "cat", "bird", "bunny", "dolphin", "tiger", "cheatah", "monkey", "human", "butterfly"]
-HALF_AWS = len(aws_list)//2
+# Lists and variables
+aws_list = ["dog", "cat", "bird", "bunny", "dolphin", "tiger", "cheetah", "monkey", "human", "butterfly", "worm", "spider", "snake"]
 AWS = random.choice(aws_list)
-NUMGUESS = random.randint(1, HALF_AWS)
+NUMGUESS = 2
+GUESS = []
+root = Tk()
+root.title("Guessing Game")
+HEIGHT = len(aws_list)*65
+WIDTH = NUMGUESS*175
+X = WIDTH // 2 - WIDTH / 10
+FONT = tkfont.Font(family = "Century", size =  15)
+#Create a background canvas
+screen = Canvas(root, width=WIDTH, height=HEIGHT, bg= "grey")
+screen.pack(fill=BOTH, expand=True)
 
-#start code
-intro_text()
-guess()
+# Feedback label
+FB = Label(root, text="", font=FONT)
+FB.place(x=10, y= HEIGHT - 100)
 
-#check if user gave the correct answer.
-y = 0
-for i in range(len(my_list)):
-    if my_list[i] == AWS:
-        y = 1
-        break
-if y==1:
-    print(f"You are correct! The correct answer was:  {AWS}")
-else:
-    print(f"Incorrect. The correct answer was:  {AWS}")
-input('Press Enter to quit')
+# Correct? Feedback
+AFB = Label(root, text="", font=FONT)
+AFB.place(x=10, y=20)
+
+#Exit Button
+EXIT = Button(root, text = "EXIT", font=FONT,command= root.destroy)
+EXIT.place(x=WIDTH-75, y=HEIGHT-50)
+
+
+# Display answer buttons.
+def display():
+    for i in range(len(aws_list)):
+        button = Button(root, text=aws_list[i], font=FONT ,command=lambda i=i: button_click(i))
+        button.place(x=X, y=80+i*50)
+
+# Convert button clicks into variables.
+def button_click(id):
+    global GUESS
+    if len(GUESS) < NUMGUESS:
+        GUESS.append(aws_list[id])
+        if len(GUESS) == NUMGUESS:
+            CHECK()
+        update_feedback()
+
+# Check answers
+def CHECK():
+    global GUESS
+    if AWS in GUESS:
+        feedback = f'Correct! You guessed: {str(AWS)}'
+    else:
+        feedback = f'Incorrect. The answer was: {str(AWS)}'
+    FEEDBACK(feedback)
+
+# Correct or wrong to user.
+def FEEDBACK(feedback):
+    AFB.config(text=feedback)
+    
+
+# Update feedback display
+def update_feedback():
+    FB.config(text=f'Your guesses: {", ".join(GUESS)}')
+
+# Start code
+display()
+root.mainloop()
