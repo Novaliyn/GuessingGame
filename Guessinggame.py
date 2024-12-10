87f8fbcb0974d398a8cba6cd3ec69b83562d6db7
Novaliyn <189510880+Novaliyn@users.noreply.github.com>
2024-12-10 15:08:50 -0800

    Added the ability of multiple lists for different answers

--- Guessinggame.py
+++ Guessinggame.py
@@ -3,44 +3,71 @@ from tkinter import *
 import tkinter.font as tkfont
 
 # Lists and variables
-aws_list = ["dog", "cat", "bird", "bunny", "dolphin", "tiger", "cheetah", "monkey", "human", "butterfly", "worm", "spider", "snake"]
-AWS = random.choice(aws_list)
-NUMGUESS = 2
+NUMGUESS = 3
 GUESS = []
+LISTS = [
+    ["Fortnite", "Roblox", "Valorant", "Minecraft", "Destiny 2", "Call of Duty"],
+    ["dog", "cat", "bird", "bunny", "dolphin", "tiger", "cheetah", "monkey", "human", "butterfly", "worm", "spider", "snake"],
+    ["Potatos", "French Fries", "Pizza", "Salad", "Sushi", "Tacos", "Burgers", "Sandwiches"],
+    ["Taco Bell", "McDonalds", "Burger King", "Subway", "Wendys", "In -N-Out", "Chick-fil-A", "KFC"],
+]
+LISTSN= ["Games", "Animals", "Food","Resturaunts"]
+LIST = []
 root = Tk()
 root.title("Guessing Game")
-HEIGHT = len(aws_list)*65
-WIDTH = NUMGUESS*175
+WIDTH = (NUMGUESS*150)+50
 X = WIDTH // 2 - WIDTH / 10
 FONT = tkfont.Font(family = "Century", size =  15)
-#Create a background canvas
-screen = Canvas(root, width=WIDTH, height=HEIGHT, bg= "grey")
-screen.pack(fill=BOTH, expand=True)
 
-# Feedback label
-FB = Label(root, text="", font=FONT)
-FB.place(x=10, y= HEIGHT - 100)
 
-# Correct? Feedback
-AFB = Label(root, text="", font=FONT)
-AFB.place(x=10, y=20)
+def start():
+    screen = Canvas(root, width=200, height=len(LISTS)*65, bg= "grey")
+    screen.pack(fill=BOTH, expand=True)
+    for i in range(len(LISTS)):
+        button = Button(root, text=LISTSN[i], font=FONT ,command=lambda i=i: LISTCHOICE(i))
+        button.place(x=10, y=20+i*50)
 
-#Exit Button
-EXIT = Button(root, text = "EXIT", font=FONT,command= root.destroy)
-EXIT.place(x=WIDTH-75, y=HEIGHT-50)
-
+def LISTCHOICE(id):
+    global LIST, AWS
+    LIST = LISTS[id]
+    AWS = random.choice(LIST)
+    for widget in root.winfo_children():
+        widget.destroy()
+    display()
 
 # Display answer buttons.
 def display():
-    for i in range(len(aws_list)):
-        button = Button(root, text=aws_list[i], font=FONT ,command=lambda i=i: button_click(i))
-        button.place(x=X, y=80+i*50)
+    global HEIGHT, FB, AFB
+    #Create a background canvas
+    HEIGHT = len(LIST)*65
+    if HEIGHT < 600:
+        HEIGHT = 600
+    screen = Canvas(root, width=WIDTH, height=HEIGHT, bg= "grey")
+    screen.pack(fill=BOTH, expand=True)
+
+
+    for f in range(len(LIST)):
+        button = Button(root, text=LIST[f], font=FONT ,command=lambda f=f: button_click(f))
+        button.place(x=X, y=80+f*50)
+
+    # Feedback label
+    FB = Label(root, text="", font=FONT)
+    FB.place(x=10, y= HEIGHT - 100)
+
+    # Correct? Feedback
+    AFB = Label(root, text="", font=FONT)
+    AFB.place(x=10, y=20)
+
+    #Exit Button
+    EXIT = Button(root, text = "EXIT", font=FONT,command= root.destroy)
+    EXIT.place(x=WIDTH-75, y=HEIGHT-50)
+
 
 # Convert button clicks into variables.
-def button_click(id):
+def button_click(id2):
     global GUESS
     if len(GUESS) < NUMGUESS:
-        GUESS.append(aws_list[id])
+        GUESS.append(LIST[id2])
         if len(GUESS) == NUMGUESS:
             CHECK()
         update_feedback()
@@ -64,5 +91,5 @@ def update_feedback():
     FB.config(text=f'Your guesses: {", ".join(GUESS)}')
 
 # Start code
-display()
+start()
 root.mainloop()
